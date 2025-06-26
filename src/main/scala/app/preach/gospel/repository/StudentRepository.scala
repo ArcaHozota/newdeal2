606c2afb1803afdce0521383c6744cb9a1243d92
ArkamaHozota <arkamahozota@gmail.com>
2025-06-26 23:15:56 +0900

    1.13

--- src/main/scala/app/preach/gospel/repository/StudentRepository.scala
+++ src/main/scala/app/preach/gospel/repository/StudentRepository.scala
@@ -5,6 +5,7 @@ import app.preach.gospel.model.Student
 
 trait StudentRepository {
   def insert(student: Student): Task[Long]
+  def update(student: Student): Task[Long]
   def findById(id: Long): Task[Option[Student]]
   def findAll(): Task[List[Student]]
 }
--- src/main/scala/app/preach/gospel/repository/impl/StudentRepositoryImpl.scala
+++ src/main/scala/app/preach/gospel/repository/impl/StudentRepositoryImpl.scala
@@ -11,16 +11,24 @@ final class StudentRepositoryImpl(ds: DataSource) extends StudentRepository {
 
   override def insert(student: Student): Task[Long] =
     run(query[Student].insertValue(lift(student)))
-      .provideEnvironment(ZEnvironment(ds))
+      .provideEnvironment(ZEnvironment(ds));
 
   override def findById(id: Long): Task[Option[Student]] =
     run(
-      query[Student].filter(s => s.id == lift(id) && s.visibleFlg == lift(true))
+      query[Student].filter(s => s.visibleFlg == lift(true) && s.id == lift(id))
     )
       .map(_.headOption)
-      .provideEnvironment(ZEnvironment(ds))
+      .provideEnvironment(ZEnvironment(ds));
 
   override def findAll(): Task[List[Student]] =
     run(query[Student].filter(_.visibleFlg == lift(true)))
-      .provideEnvironment(ZEnvironment(ds))
+      .provideEnvironment(ZEnvironment(ds));
+
+  override def update(student: Student): Task[Long] =
+    run(
+      query[Student]
+        .filter(s => s.visibleFlg == lift(true) && s.id == lift(student.id))
+        .updateValue(lift(student))
+    )
+      .provideEnvironment(ZEnvironment(ds));
 }
